8a0ed98531c769d70bff346e7ed0f19d23273f5a
Mathias Novais <117697360+west3r@users.noreply.github.com>
2024-06-05 21:10:20 -0300

    criando_banco_de_dados.py

--- criando_banco_de_dados.py
+++ criando_banco_de_dados.py
@@ -1,65 +1,57 @@
-#criando_banco_de_dados.py
 import sqlite3
 
-# CRIANDO CONEXAO
+# Criando conexão
 try:
     con = sqlite3.connect('cadastro_alunos.db')
-    print('Conexão com banco de dados realizada com sucesso!')
+    print("Conexão com o banco de dados realizada com sucesso!")
 except sqlite3.Error as e:
-    print("Erro ao conectar com banco de dados:", e)
+    print("Erro ao conectar com o banco de dados:", e)
 
-#criando Tabela de cursos
+# Criando a tabela de cursos
 try:
     with con:
         cur = con.cursor()
-        cur.execute(""" CREATE TABLE IF NOT EXISTS cursos(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT,
-            duracao TEXT,
-            preco REAL                      
-        )""" )
-
-        print("Tabela Cursos criado com sucesso!")
-
+        cur.execute("""CREATE TABLE IF NOT EXISTS cursos (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nome TEXT,
+                        duracao TEXT,
+                        preco REAL
+                    )""")
+        print("Tabela cursos criada com sucesso!")
 except sqlite3.Error as e:
     print("Erro ao criar a tabela cursos:", e)
 
-# Criando tabela de turmas
+# Criando a tabela de turmas
 try:
     with con:
         cur = con.cursor()
-        con.execute(""" CREATE TABLE IF NOT EXISTS turmas(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT,
-            curso_nome TEXT,
-            data_inicio DATE,
-            FOREIGN KEY (cursos_nome) REFERENCES cursos (nome) ON UPDATE CASCADE ON DELETE CASCADE      
-        )""")
-
-        print("Tabela Turmas criada com sucesso!")
-
-except sqlite3.Erro as e:
-    print("Erro ao criar a tabela turmas: ", e)
-
-# Criando tabela de alunos
-try:
-    with con:
-        cur = con.cursor()
-        con.execute(""" CREATE TABLE IF NOT EXISTS alunos(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT,
-            email TEXT,
-            telefone TEXT,
-            sexo TEXT,
-            imagem TEXT,
-            data_nascimento DATE,
-            cpf TEXT,
-            turma_nome TEXT,
-            FOREIGN KEY (turma_nome) REFERENCES turmas (nome) ON DELETE CASCADE
-        )""")
-
-        print("Tabela Alunos criada com sucesso!")
-
+        cur.execute("""CREATE TABLE IF NOT EXISTS turmas (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nome TEXT,
+                        cursos_nome TEXT,
+                        data_inicio DATE,
+                        FOREIGN KEY (cursos_nome) REFERENCES cursos (nome) ON UPDATE CASCADE ON DELETE CASCADE
+                    )""")
+        print("Tabela turmas criada com sucesso!")
 except sqlite3.Error as e:
-    print("Erro ao criar a tabela Alunos:", e)
-    
+    print("Erro ao criar a tabela turmas:", e)
+
+# Criando a tabela de alunos
+try:
+    with con:
+        cur = con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS alunos (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nome TEXT,
+                        email TEXT,
+                        telefone TEXT,
+                        sexo TEXT,
+                        imagem TEXT,
+                        data_nascimento DATE,
+                        cpf TEXT,
+                        turma_nome TEXT,
+                        FOREIGN KEY (turma_nome) REFERENCES turmas (nome) ON DELETE CASCADE
+                    )""")
+        print("Tabela alunos criada com sucesso!")
+except sqlite3.Error as e:
+    print("Erro ao criar a tabela alunos:", e)
